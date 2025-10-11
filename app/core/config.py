94fbed4527fccc83cd30b6868587954c7979683b
OSpoon <zxin088@gmail.com>
2025-10-11 17:41:38 +0800

    feat(cors): 添加CORS支持以允许跨域请求
    在配置文件中添加BACKEND_CORS_ORIGINS环境变量
    在docker-compose中注入CORS配置
    在FastAPI应用中添加CORSMiddleware中间件

--- app/core/config.py
+++ app/core/config.py
@@ -2,13 +2,23 @@
 应用程序设置
 """
 
-from typing import Literal
+from typing import Annotated, Any, Literal
 
 from pydantic import (
+    AnyUrl,
+    BeforeValidator,
     PostgresDsn,
     computed_field,
 )
 from pydantic_settings import BaseSettings
+
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",") if i.strip()]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 
 class Settings(BaseSettings):
@@ -37,6 +47,15 @@ class Settings(BaseSettings):
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
         )
+
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS]
 
 
 settings = Settings()  # type: ignore
--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from api_exception import (
     register_exception_handlers,
 )
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_router_v1
 from app.core.config import settings
@@ -25,6 +26,16 @@ app = FastAPI(
     version=settings.API_VERSION,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
+
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(api_router_v1, prefix=settings.API_V1_STR)
 
